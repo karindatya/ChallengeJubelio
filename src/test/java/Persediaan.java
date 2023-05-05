8ec82c954d56495993c9ab1a806fa01df324216e
Karin <karindatya93@gmail.com>
2023-05-05 09:44:56 +0700

    init

--- src/test/java/Persediaan.java
+++ src/test/java/Persediaan.java
@@ -1,12 +1,16 @@
 import config.env_target;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class Persediaan extends env_target {
     @Test
